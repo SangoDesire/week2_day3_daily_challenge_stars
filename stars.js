994b00f2e04b56b7b171ddf046fcac6a6480c77d
analysteprodez <analysteprodez@gmail.com>
2022-12-29 23:02:07 +0000

    week2 day3 daily challenge stars

---
+++ stars.js
@@ -0,0 +1,31 @@
+//Write a JavaScript program that recreates the pattern below.
+
+for (let i = 0; i <= 5; i++) {
+    console.log(' * '.repeat(i + 1));
+}
+
+// Do this challenge twice: first by using one loop, then by using two nested for ...................
+
+
+let a = " "
+
+for (let i = 0; i <= 5; i++) {
+
+    a = a + " * "
+    console.log(a)
+}
+
+
+for (let u = 0; u <= 5; n++) {
+    const r = +1
+    let b = " "
+
+    for (let t = 0; t <= r; v++) {
+        b = b + " * "
+    }
+    console.log(b)
+
+
+}
+
+console.log('good')
